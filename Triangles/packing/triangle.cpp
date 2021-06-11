18056fc7b4c60d0d7acedcdffd98f798f2bb1bc0
Anacharsis <voinov.forever@gmail.com>
2021-06-11 09:38:36 +0300

    Fixed deg_2_rad

--- Triangles/packing/triangle.cpp
+++ Triangles/packing/triangle.cpp
@@ -5,6 +5,7 @@ namespace packing
 	using Vec2d = Eigen::Vector2d;
 
 	constexpr double sqrt_3 = 1.73205080757;
+	constexpr double deg_2_rad = EIGEN_PI / 180;
 
 	double Triangle::size() const
 	{
@@ -24,21 +25,21 @@ namespace packing
 	Vec2d Triangle::point_a() const
 	{
 		Vec2d shift = size_ * Vec2d{ -0.5, -sqrt_3/6 };
-		shift = Eigen::Rotation2D(alpha_) * shift;
+		shift = Eigen::Rotation2D(alpha_ * deg_2_rad) * shift;
 		return position_ + shift;
 	}
 
 	Vec2d Triangle::point_b() const
 	{
 		Vec2d shift = size_ * Vec2d{ 0, sqrt_3 / 3 };
-		shift = Eigen::Rotation2D(alpha_) * shift;
+		shift = Eigen::Rotation2D(alpha_ * deg_2_rad) * shift;
 		return position_ + shift;
 	}
 
 	Vec2d Triangle::point_c() const
 	{
 		Vec2d shift = size_ * Vec2d{ 0.5, -sqrt_3 / 6 };
-		shift = Eigen::Rotation2D(alpha_) * shift;
+		shift = Eigen::Rotation2D(alpha_ * deg_2_rad) * shift;
 		return position_ + shift;
 	}
 
@@ -55,24 +56,24 @@ namespace packing
 	void Triangle::set_point_a(const Vec2d& position, double alpha)
 	{
 		alpha_ = alpha;
-		Vec2d shift = size_ * Vec2d{ -0.5, -sqrt_3 / 6 };
-		shift = Eigen::Rotation2D(alpha) * shift;
+		Vec2d shift = size_ * Vec2d{ 0.5, sqrt_3 / 6 };
+		shift = Eigen::Rotation2D(alpha * deg_2_rad) * shift;
 		position_ = position + shift;
 	}
 
 	void Triangle::set_point_b(const Vec2d& position, double alpha)
 	{
 		alpha_ = alpha;
-		Vec2d shift = size_ * Vec2d{ 0, sqrt_3 / 3 };
-		shift = Eigen::Rotation2D(alpha) * shift;
+		Vec2d shift = size_ * Vec2d{ 0, - sqrt_3 / 3 };
+		shift = Eigen::Rotation2D(alpha * deg_2_rad) * shift;
 		position_ = position + shift;
 	}
 
 	void Triangle::set_point_c(const Vec2d& position, double alpha)
 	{
 		alpha_ = alpha;
-		Vec2d shift = size_ * Vec2d{ 0.5, -sqrt_3 / 6 };
-		shift = Eigen::Rotation2D(alpha) * shift;
+		Vec2d shift = size_ * Vec2d{ -0.5, sqrt_3 / 6 };
+		shift = Eigen::Rotation2D(alpha * deg_2_rad) * shift;
 		position_ = position + shift;
 	}
 }

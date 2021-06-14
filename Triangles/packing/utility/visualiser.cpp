8cdca7a84a64a7f53a472ec1ac85f4b33b218b1e
Anacharsis <voinov.forever@gmail.com>
2021-06-14 16:21:26 +0300

    Added chain display, fixed to_cv_()

--- Triangles/packing/utility/visualiser.cpp
+++ Triangles/packing/utility/visualiser.cpp
@@ -1,35 +1,61 @@
 #include <packing/utility/visualiser.h>
 
 #include <vector>
+#include <optional>
 #include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
 #include <packing/triangle.h>
 #include <Eigen/Dense>
 
 namespace packing::utility
 {
+	void StripVisualiser::display_fitting_chain_(
+		cv::Mat img, 
+		const std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>>& chain,
+		const cv::Scalar& color)
+	{
+		for(auto& segment : chain)
+		{
+			cv::line(img, to_cv_(segment.first), to_cv_(segment.second), color, 1, 8);
+		}
+	}
+
 	void StripVisualiser::display_strip(
 		const std::string& filepath,
 		double width,
 		double height,
-		const std::vector<Triangle>& packing
+		const std::vector<Triangle>& packing,
+		const std::optional<std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>>>& fitting_chain
 	)
 	{
-		cv::Mat img = cv::Mat::zeros(static_cast<int>(stripe_to_pixel_scale_) * width, height * static_cast<int>(stripe_to_pixel_scale_), CV_8UC3);
+		current_width_ = static_cast<int>(stripe_to_pixel_scale_) * width;
+		current_height_= static_cast<int>(stripe_to_pixel_scale_) * height;
+		cv::Mat img =
+			cv::Mat::ones(current_height_, current_width_, CV_8UC3);
+
 		for(auto& triangle : packing)
 		{
 			draw_triangle_(std::move(img), triangle);
 		}
 
-		cv::imshow("img", img);
+		if(fitting_chain)
+		{
+			cv::Mat img = 
+				cv::Mat::ones(current_height_, current_width_, CV_8UC3);
+			display_fitting_chain_(std::move(img), fitting_chain.value());
+			cv::imshow("chain", img);
+			cv::waitKey(0);
+		}
+
+		cv::imshow("stripe", img);
 		cv::waitKey(0);
 		cv::destroyAllWindows();
 	}
 
 	void StripVisualiser::draw_triangle_(cv::Mat img, const Triangle& triangle)
 	{
-		cv::polylines(img, to_cv_(triangle), true, cv::Scalar(255, 255, 255), 2, 8);
+		cv::polylines(img, to_cv_(triangle), true, cv::Scalar(205, 0, 0), 1, 8);
 	}
 
 	std::vector<cv::Point> StripVisualiser::to_cv_(const Triangle& triangle) const
@@ -37,8 +63,18 @@ namespace packing::utility
 		return std::vector<cv::Point>{ to_cv_(triangle.point_a()), to_cv_(triangle.point_b()), to_cv_(triangle.point_c())};
 	}
 
+	std::vector<cv::Point> StripVisualiser::to_cv_(const std::vector<Eigen::Vector2d>& vec) const
+	{
+		std::vector<cv::Point> cv_points;
+		for(auto& point : vec)
+		{
+			cv_points.push_back(to_cv_(point));
+		}
+		return cv_points;
+	}
+
 	cv::Point StripVisualiser::to_cv_(const Eigen::Vector2d& vec) const
 	{
-		return cv::Point{ static_cast<int>(vec.x() * stripe_to_pixel_scale_), static_cast<int>(vec.y() * stripe_to_pixel_scale_) };
+		return cv::Point{ static_cast<int>(vec.x() * stripe_to_pixel_scale_), current_height_ - static_cast<int>(vec.y() * stripe_to_pixel_scale_) };
 	}
 }
--- Triangles/packing/utility/visualiser.h
+++ Triangles/packing/utility/visualiser.h
@@ -3,6 +3,8 @@
 #include <packing/triangle.h>
 #include <opencv2/core.hpp>
 #include <Eigen/Dense>
+#include <optional>
+#include <utility>
 
 namespace packing::utility
 {
@@ -11,7 +13,9 @@ namespace packing::utility
 	public:
 		explicit StripVisualiser(double stripe_to_pixel_scale)
 			:
-			stripe_to_pixel_scale_(stripe_to_pixel_scale)
+			stripe_to_pixel_scale_(stripe_to_pixel_scale),
+			current_width_(0),
+			current_height_(0)
 		{
 		}
 
@@ -23,15 +27,23 @@ namespace packing::utility
 			const std::string& filepath,
 			double width,
 			double height,
-			const std::vector<packing::Triangle>& packing
+			const std::vector<packing::Triangle>& packing,
+			const std::optional<std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>>>& fitting_chain
 		);
 
 	private:
 		void draw_triangle_(cv::Mat img, const Triangle&);
+		void display_fitting_chain_(
+			cv::Mat img,
+			const std::vector<std::pair<Eigen::Vector2d, Eigen::Vector2d>>& chain,
+			const cv::Scalar& color = cv::Scalar{ 0, 205, 0 });
 
 		std::vector<cv::Point> to_cv_(const Triangle&) const;
+		std::vector<cv::Point> to_cv_(const std::vector<Eigen::Vector2d>&) const;
 		cv::Point to_cv_(const Eigen::Vector2d&) const;
 	private:
 		double stripe_to_pixel_scale_;
+		int current_width_;
+		int current_height_;
 	};
 }
